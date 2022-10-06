ab1ba5372b5418a3b815f1f58d161c4045689064
Danil <dg444k@gmail.com>
2022-10-06 23:23:40 +0300

    add new functional

--- script.js
+++ script.js
@@ -1,78 +1,41 @@
+let firstNum = '';
+let lastNum = '';
+let operator = '';
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const operators = ['*', '-', '+', '√', '÷'];
+
 const display = document.querySelector(".display_calculator");
-const group_btn  = document.querySelector(".group_btn");
-const numbers = document.querySelectorAll("*[data-number]");
-const operators = document.querySelectorAll("*[data-operator]");
-const dot = document.querySelector(".dot");
-const equal = document.querySelector(".equals");
-const sqaure = document.querySelector(".sqrt");
-const ac = document.querySelector(".ac");
-const ce = document.querySelector(".ce");
 
 
+document.getElementById('ac').onclick = function()
+{
+    firstNum = '';
+    lastNum = '';
+    operator = '';
 
-group_btn.onclick = function(e){
-    let target = e.target;
+    display.textContent = '';
+}
 
-    if(target.classList != "btn"){
-        return;
+document.querySelector('.group_btn').onclick = function(e)
+{
+    if(!e.target.classList.contains('btn')) {return;}
+
+    if(e.target.id == 'ac') {return;}
+   
+    let key = e.target.textContent;
+
+    if(numbers.includes(key))
+    {
+        firstNum += key;
+        display.textContent = firstNum;
     }
 
-    show(target);
-}
+    if(operators.includes(key))
+    {
+        operator = key;
+        display.textContent = operator;
 
-
-ac.addEventListener("click", clearAll);
-ce.addEventListener("click", clearElem);
-
-
-
-
-
-function show(elem){
-    display.innerHTML += elem.innerHTML;
-    
-}
-
-function clearAll() {
-    display.innerHTML = "";
-}
-
-function clearElem() {
-    let str = display.innerHTML;
-    let lengthString = str.length;
-    display.innerHTML = str.slice(0, lengthString-1);
-}
-
-function sum(a, b) {
-    return a + b;
-}
-
-function residual(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function division(a, b) {
-    return a / b;
-}
-
-function sqrt(a) {
-    return Math.sqrt(a);
-}
-
-
-function operate(a, b, operator){
-    switch (operator) {
-        case "+":
-            return a + b;
-        case "-":
-            return a - b;
-        case "/":
-            return a / b;
-        case "*":
-            return a * b;
     }
+
 }
