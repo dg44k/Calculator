55d2f58f22b09d832193550cb3ae7c8a131171c6
Danil <dg444k@gmail.com>
2022-10-07 22:30:52 +0300

    add function count

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let lastNum = '';
 let operator = '';
 
 const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const operators = ['*', '-', '+', '√', '÷'];
+const operators = ['*', '-', '+', '÷'];
 
 const display = document.querySelector(".display_calculator");
-
+const equal = document.getElementById('equals');
 
 document.getElementById('ac').onclick = function()
 {
@@ -21,21 +21,70 @@ document.querySelector('.group_btn').onclick = function(e)
 {
     if(!e.target.classList.contains('btn')) {return;}
 
+
     if(e.target.id == 'ac') {return;}
    
+
     let key = e.target.textContent;
+    let inter_value = '';
+
+    if(lastNum != '' && firstNum != '' && operators.includes(key))
+    {
+        let o = operate(firstNum,lastNum, operator);
+        display.textContent = o;
+        firstNum = o;
+        operator = key;
+        lastNum = '';
+    }
+
 
     if(numbers.includes(key))
     {
-        firstNum += key;
-        display.textContent = firstNum;
+        if(operator == '' && lastNum == '')
+        {
+            firstNum += key;
+            display.textContent = firstNum;
+        }
+
+        else
+        {
+            inter_value = display.textContent;
+            lastNum += key;
+            display.textContent = inter_value + lastNum;
+        }
     }
+
 
     if(operators.includes(key))
     {
         operator = key;
-        display.textContent = operator;
+        display.textContent += operator;
 
     }
 
+
+    if(key == equal.textContent)
+    {
+        let r = operate(firstNum, lastNum, operator);
+        if(lastNum == '') lastNum = firstNum;
+        display.textContent = r;
+        firstNum = r;
+        lastNum = '';
+
+    }
+
+}
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return Number(a)+Number(b);
+        case '-':
+            return Number(a)-Number(b);
+        case '*':
+            return Number(a)*Number(b);
+        case '÷':
+            return Number(a)/Number(b);
+
+    }
 }
